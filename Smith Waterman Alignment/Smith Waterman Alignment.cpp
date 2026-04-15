3b5f4c1e347e5e4f362fcf4e99ccbdfd4c367377
gabufle <fsgabriel6@gmail.com>
2026-04-14 21:21:35 -0700

    Added the scoring math and added comments

--- Smith Waterman Alignment/Smith Waterman Alignment.cpp
+++ Smith Waterman Alignment/Smith Waterman Alignment.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 int main() {
 	std::string seq1 = "CGTGAATTCG";
@@ -31,12 +32,45 @@ int main() {
 
 	std::cout << "matrix successfully made in memory: " << rows << " rows by: " << cols << " columns" << std::endl;
 
+	
+
+	// make the scoring system 
+	//keep track of the max score
+	int max_score = 0; 
+
+	for (int i = 1; i < rows; i++) {
+		for (int j = 1; j < cols; j++) {
+
+			//Option 1: Diagonal match or mismatch
+			int diagonal_score = matrix[i - 1][j - 1];
+			if (seq2[i - 1] == seq1[j - 1]) {
+				diagonal_score += match_score; 
+			}
+			else {
+				diagonal_score += mismatch_pen; 
+			}
+
+			// options 2 and 3: Gap conditions
+			int up_score = matrix[i - 1][j] + gap_pen; 
+			int left_score = matrix[i][j - 1] + gap_pen; 
+
+			// highest score
+			matrix[i][j] = std::max({ 0, diagonal_score, up_score, left_score }); 
+
+			//Highest total score overall grid
+			if (matrix[i][j] > max_score) {
+				max_score = matrix[i][j];
+			}
+
+		}
+	}
+	std::cout << "The highest local alignment score is: " << max_score << std::endl;
+
 	//memory cleanup
 	for (int i = 0; i < rows; i++) {
 		delete[] matrix[i];
 	}
 	delete[] matrix;
-
 
 	return 0;
 }
